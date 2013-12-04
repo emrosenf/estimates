5a0e785599d3a54694a7492592440612f31ad691
Evan Rosenfeld <evan@rescomp-12-252359.local>
2013-12-03 18:50:57 -0800

    Initial Commit

---
+++ Estimates/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Estimates
+//
+//  Created by Evan Rosenfeld on 12/3/13.
+//  Copyright (c) 2013 Everpilot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
