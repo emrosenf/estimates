7669c66b9cde1e2018386ad383979e79717d7537
Nick Lane-Smith <nlanesmith@gmail.com>
2013-12-10 17:58:46 -0800

    adding in Aviary and image edit capabilities

---
+++ Estimates/ImageEstimateTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageEstimateTableViewController.h
+//  Estimates
+//
+//  Created by Nick Lane-Smith on 12/10/13.
+//  Copyright (c) 2013 Everpilot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageEstimateTableViewController : UITableViewController
+
+@end
---
+++ Estimates/ImageLineItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageLineItemViewController.h
+//  Estimates
+//
+//  Created by Nick Lane-Smith on 12/10/13.
+//  Copyright (c) 2013 Everpilot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageLineItemViewController : UITableViewController
+
+@end
